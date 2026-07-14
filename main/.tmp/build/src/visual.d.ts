7ea8cdf6eb49bd32098464f3aac2f3decd980b75
Phikolomzi Gugwana <ggwphi001@myuct.ac.za>
2026-07-14 10:42:31 +0200

    Added basic wireframe (#3)
    Co-authored-by: Phikolomzi Gugwana <PGugwana2@oldmutual.com>

---
+++ main/.tmp/build/src/visual.d.ts
@@ -0,0 +1,24 @@
+import powerbi from "powerbi-visuals-api";
+import IVisual = powerbi.extensibility.visual.IVisual;
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+export declare class Visual implements IVisual {
+    private readonly hostElement;
+    private readonly root;
+    private readonly styleElement;
+    constructor(options: VisualConstructorOptions);
+    update(options: VisualUpdateOptions): void;
+    private render;
+    private renderHeader;
+    private renderWorkspace;
+    private renderFieldWells;
+    private renderWell;
+    private renderPivotArea;
+    private renderTableWireframe;
+    private renderFieldsPanel;
+    private renderFieldGroup;
+    private renderFieldItem;
+    private createButton;
+    private createElement;
+    private getStyles;
+}
