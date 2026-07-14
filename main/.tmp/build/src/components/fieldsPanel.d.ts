56397c5a6bd433b4765baa893b89200bd9da3c69
Phikolomzi Gugwana <ggwphi001@myuct.ac.za>
2026-07-14 16:31:44 +0200

    styled the field list (#6)
    Co-authored-by: Phikolomzi Gugwana <PGugwana2@oldmutual.com>

--- main/.tmp/build/src/components/fieldsPanel.d.ts
+++ main/.tmp/build/src/components/fieldsPanel.d.ts
@@ -4,15 +4,9 @@ export type FieldDataType = "date" | "number" | "text" | "boolean" | "geography"
 export interface AnalyticsField {
     id: string;
     name: string;
-    groupName: string;
     type: FieldDataType;
     isMeasure: boolean;
     selected: boolean;
-}
-export interface FieldGroup {
-    name: string;
-    icon: string;
-    fields: AnalyticsField[];
 }
 export interface FieldsPanelOptions {
     fields: AnalyticsField[];
